d1af0c44b39fd0f966e06a132cced2c9bb7db466
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2021-06-06 19:27:23 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="corps">
+  <div class="todo">
+  <h1>Todo list</h1>
+  <input type="text" v-model="todo" @keyup.enter="pushTodo(todo)">
+  <button @click="pushTodo(todo)">Add Todo</button>
+  </div>
+  <!-- <p>{{todos}}</p> -->
+  <todolist :todos="todos"/>
+</div>
+  
+  
+</template>
+
+<script>
+import todolist from './components/todolist.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'todolist':todolist,
+  },
+  data(){
+    return {
+      todos :[],
+      todo:'',
+    }
+  },
+  methods:{
+    pushTodo(todo){
+      console.log(todo);
+      if (todo===''){
+        window.alert('please enter todo');
+      } else {
+      this.todos.push(todo); }
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  margin : 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+body {
+  background-color: #27292d;
+  height: 100%;
+}
+.corps {
+  width: 100vw;
+  
+  background-color: #27292d;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.todo {
+  width: 33%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border: 3px solid #727476;
+  border-radius: 10px;
+  height: 10%;
+  margin-top: 3vh;
+}
+.todo h1 {
+  align-self: flex-start;
+}
+
+.todo input {
+  width: 400px;
+  border-radius: 10px;
+  height: 3vh;
+}
+button{
+  width: 40%;
+  background-color: #a0a4d9;
+  color: black;
+  height: 30%;
+  border-radius: 6px;
+  font-size: 1.2em;
+}
+</style>
---
+++ src/components/todolist.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="todoCard" v-for="(todo,index) in todos" :key="todo" >
+        <p>{{todos[index]}}</p>
+        <button @click="eraseTodo(index)">erase</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name : 'todolist',
+    data(){
+        return{
+            index:0,
+        }
+    },
+    props : ['todos'],
+    methods:{
+        //splice(index,1)
+        eraseTodo(index) {
+            this.$props.todos.splice(index,1);
+        }
+    },
+    
+}
+
+</script>
+
+<style>
+.todoCard {
+    width: 33%;
+    height: 60px;
+    border: 3px solid #727476;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 2%;
+}
+
+.todoCard button {
+    width: 22%;
+    height: 50%;
+}
+</style>
